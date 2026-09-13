69f1179845515bc9f5ada3804fa421f134445b4a
khushi-gtf53 <khushi.kashyap@gtftechnologies.com>
2026-09-13 17:05:36 +0530

    update cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-import brochurePdf from "../assets/brochure/khushi.pdf";
+import brochurePdf from "../assets/brochure/Khushi_Kashyap_ThreeJS_Developer.pdf";
 import BallParticle from "./canvas/BallParticle";
 import Setup from "./canvas/setup/Setup";
 
